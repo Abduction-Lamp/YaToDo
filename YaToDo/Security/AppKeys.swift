dce41321bee1b5c2a952f96a60d560a01cd999a7
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2022-11-14 15:31:52 +0300

    configuring the project folder structure

---
+++ YaToDo/Security/AppKeys.swift
@@ -0,0 +1,15 @@
+//
+//  AppKeys.swift
+//  YaToDo
+//
+//  Created by Владимир on 14.11.2022.
+//
+
+import Foundation
+
+struct AppKeys {
+    
+    static let shared = AppKeys()
+    private init() { }
+    
+}
